909497204460ec34065a24d00c094c4fb4973355
Adam <adam.keristoper@gmail.com>
2019-01-13 20:38:35 +0900

    Update bash script to support linux

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-[[ -z "$1" ]] && { echo "No target service, exiting" ; exit 1; }
+if [ -z "$1" ]
+  then
+    echo "No target service, exiting"
+    exit 1
+fi
 docker-compose build $1 \
 && docker-compose up --no-deps -d $1 \
 && yes | docker image prune
